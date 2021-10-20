8d3269e6d79e10740f99b5dd17d753301268c642
salvi-siddhi333 <siddhi.salvi@sakec.ac.in>
2021-10-20 20:54:08 +0530

    added setup and procfile

---
+++ repos.py
@@ -0,0 +1,93 @@
+# import libraries
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+import numpy as np
+
+def trends():
+    #Url of website
+    url = "https://github.com/trending"
+
+    response = requests.get(url)
+    # print(response.status_code)
+
+    #Check if website is UP and running well!
+    if(response.status_code) != 200:
+        print("Error!")
+
+    #Getting Content
+    content = response.text
+    #print(type(content))
+    soup = bs(content,"html.parser")
+    #print(soup)
+
+    #article_tag = soup.find_all("article", {"class": "Box-row"})
+    # print(article_tag)
+
+    #extracting all <a> from <h1> 
+    blk_details = soup.find_all("article", {"class": "Box-row"})
+
+    #List of different details
+    author = []
+    repo = []
+    lang = []
+    star = []
+    fork = []
+
+    #iterate each block
+    for i in range(len(blk_details)):
+        #Extract Author and Repo
+        title = blk_details[i].find_all("h1", {"class": "h3 lh-condensed"})[0].text
+        title = title.strip().replace(" ", "")
+        val_list = title.split("/")
+        for val in val_list:
+            val.replace("\n", "")
+        author.append(val_list[0])
+        repo.append(val_list[1][2:])
+
+
+        #Extract Language
+        lang_lst = blk_details[i].find_all("span", {"itemprop": "programmingLanguage"})
+        if(lang_lst):
+            lang.append(lang_lst[0].text)
+        else:
+            lang.append(np.nan)
+
+        #Extract Star
+        star_lst = blk_details[i].find_all("a", {"class": "Link--muted d-inline-block mr-3"})
+        #star = star_lst[0].strip().replace(" ", "")
+        if(star_lst):
+            star.append(star_lst[0].text.strip())
+        else:
+            star.append(np.nan)
+
+
+        #Extract Fork
+        fork_lst = blk_details[i].find_all("a", {"class": "Link--muted d-inline-block mr-3"})
+        if(fork_lst):
+            fork.append(fork_lst[1].text.strip())
+        else:
+            fork.append(np.nan)
+
+    trending_dict = {
+        "Author": author,
+        "Repo" : repo, 
+        "Language" : lang,
+        "Star" : star,
+        "Fork" : fork
+    }
+
+    #Created Data frame 
+    trending_dict = pd.DataFrame(trending_dict)
+    #Converted to csv
+    trending_dict.to_csv("Trending repositories on GitHub.csv")
+    # print(trending_dict.head())
+    return trending_dict
+
+    # print("Details of Trending Repositories")
+    # for i in range(len(author)):
+    #     print(author[i], "==>", repo[i])
+    #     print("Language ==>", lang[i])
+    #     print("Star ==>", star[i])
+    #     print("Fork ==>", fork[i])
+    #     print("_"*50)
--- topics.py
+++ topics.py
@@ -4,52 +4,54 @@ from bs4 import BeautifulSoup as bs
 import numpy as np
 import pandas as pd
 
-#URL of the scrapping website
-url = "https://github.com/topics"
+def topics():
+    #URL of the scrapping website
+    url = "https://github.com/topics"
 
-#GETTING url
-response = requests.get(url)
-print(response.status_code)
+    #GETTING url
+    response = requests.get(url)
+    # print(response.status_code)
 
-#Checking if URL is perfectly running
-if(response.status_code != 200):
-    print("Error")
+    #Checking if URL is perfectly running
+    if(response.status_code != 200):
+        print("Error")
 
-#GETTING content
-content = response.text
+    #GETTING content
+    content = response.text
 
-soup = bs(content,"html.parser")
+    soup = bs(content,"html.parser")
 
-blk = soup.find_all("div", {"class" : "py-4 border-bottom"})
+    blk = soup.find_all("div", {"class" : "py-4 border-bottom"})
 
-#Details
-title = []
-content = []
+    #Details
+    title = []
+    content = []
 
-#iterating block
-for i in range(len(blk)):
-    #Extract title
-    title_lst = blk[i].find_all("p", {"class": "f3 lh-condensed mb-0 mt-1 Link--primary"})
-    if(title_lst):
-        title.append(title_lst[0].text)
-    else:
-        title.append(np.nan)
+    #iterating block
+    for i in range(len(blk)):
+        #Extract title
+        title_lst = blk[i].find_all("p", {"class": "f3 lh-condensed mb-0 mt-1 Link--primary"})
+        if(title_lst):
+            title.append(title_lst[0].text)
+        else:
+            title.append(np.nan)
 
-    content_lst = blk[i].find_all("p", {"class": "f5 color-text-secondary mb-0 mt-1"})
-    # content_lst = content_lst.replace
-    if(content_lst):
-        for val in content_lst[0]: 
-            val = val.replace("\n", "").strip()
-        # print(content_lst[0])
-        # content_lst[0] = content_lst[0].replace("\n", "")
-            content.append(val)
-    else:
-        content.append(np.nan)
+        content_lst = blk[i].find_all("p", {"class": "f5 color-text-secondary mb-0 mt-1"})
+        # content_lst = content_lst.replace
+        if(content_lst):
+            for val in content_lst[0]: 
+                val = val.replace("\n", "").strip()
+            # print(content_lst[0])
+            # content_lst[0] = content_lst[0].replace("\n", "")
+                content.append(val)
+        else:
+            content.append(np.nan)
 
-df = {"Title" : title, "Content" : content}
-#converting into DataFrame
-df = pd.DataFrame(df)
-df.to_csv("topics.csv")
+    df = {"Title" : title, "Content" : content}
+    #converting into DataFrame
+    df = pd.DataFrame(df)
+    # df.to_csv("topics.csv")
+    return df
 
-# print("Title ==> ", title)
-# print("Content ==>", content)
+    # print("Title ==> ", title)
+    # print("Content ==>", content)
